f04d9bd2b03738fa183ef81d8ba4d90a152e0364
pata1202 <jhjs1202@hanyang.ac.kr>
2024-11-18 22:02:12 +0900

    BJH_api

--- dog-info-app/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('./userController');
-
-router.post('/api/user', userController.saveUserInfo);
-router.get('/api/user/:id', userController.getUserInfo);
-
-module.exports = router;
