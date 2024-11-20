be540509e052b5550cc5f75b4c8761589d0947ce
LeeDongRyul57 <ga0212kimi@naver.com>
2024-11-20 18:31:36 +0900

    프론트코드

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Loading1 from './dogAlonePage/Loading1'; // loading 1 페이지 가져오기
-import Login from './dogAlonePage/Login'; // Login 컴포넌트 가져오기
-import smartHome from './dogAlonePage/smartHome'; // 스마트홈 컴포넌트 가져오기
+import Loading1 from './dogAlonePage/Loading1';
+import Login from './dogAlonePage/Login'; 
+import SmartHome from './dogAlonePage/SmartHome'; 
+import MainPage from './dogAlonePage/MainPage';
+import ReportPage from './dogAlonePage/ReportPage';
+import DocuPage from './dogAlonePage/DocuPage';
+import RegisterPage from './dogAlonePage/RegisterPage';
 import { View } from 'react-native';
 
-const Stack = createStackNavigator();  // Stack 객체 생성
+const Stack = createStackNavigator();  
 
 export default function App() {
   
-  return ( // return을 추가하여 JSX를 반환합니다.
+  return ( 
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="smartHome" component={smartHome} />
         <Stack.Screen name="Loading" component={Loading1} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="RegisterPage" component={RegisterPage} />
+        <Stack.Screen name="MainPage" component={MainPage} />
+        <Stack.Screen name="smartHome" component={SmartHome} />
+        <Stack.Screen name="ReportPage" component={ReportPage} />
+        <Stack.Screen name="DocuPage" component={DocuPage} />
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
