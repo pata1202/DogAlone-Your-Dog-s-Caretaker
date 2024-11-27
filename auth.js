eafca9758ef45aed4ad3742fcfb2595aeaa41c3c
Seoyeon Kim <2001seoyeon@gmail.com>
2024-11-27 21:36:51 +0900

    최종 연동

--- auth.js
+++
@@ -1,7 +0,0 @@
-import { getAuth } from "firebase/auth";
-import { app } from "./firebase"; // firebase.js의 경로에 맞게 수정
-
-// Firebase 인증 초기화
-const auth = getAuth(app);
-
-export default auth;
---
+++ backend/food.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql2");
+const connection = require('../backend/config/db');
+
+// 데이터베이스 연결
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL!");
+});
+
+// 급식 스케줄 테이블 생성
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS FeedingSchedule (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    feeding_time VARCHAR(10) NOT NULL,
+    day_of_week VARCHAR(10) NOT NULL,
+    feeding_count INT NOT NULL,
+    status ENUM('ON', 'OFF') DEFAULT 'ON'
+  )
+`;
+
+connection.query(createTableQuery, (err, results) => {
+  if (err) {
+    console.error("Error creating table:", err);
+    return;
+  }
+  console.log("Table created or already exists.");
+});
+
+// 연결 종료
+connection.end();
