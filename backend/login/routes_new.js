bb69fd4146d0563b2eda798b5c146a82765bdbb1
Seoyeon Kim <2001seoyeon@gmail.com>
2024-11-28 00:32:13 +0900

    "new routes"

--- backend/login/routes_new.js
+++ backend/login/routes_new.js
@@ -6,6 +6,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("./authController");
+const recommendation = require("./recommendation")
+const report_daily = require("./report_daily")
+const report_weekly = require("./report_weekly")
+const report_monthly = require("./report_monthly")
 
 // 회원가입 엔드포인트
 router.post("/api/register", authController.register);
@@ -15,5 +19,14 @@ router.post("/api/login", authController.login);
 
 // 사용자 정보 호출 엔드포인트
 router.get("/api/user/:userId", authController.getUserInfo);
+
+// 시스템 추천
+router.get("/recommendation", recommendation)
+
+// 울음리포트 일, 주, 월별 
+router.get("/report.daily", report_daily)
+router.get("/report.weekly", report_weekly)
+router.get("/report.monthly", report_monthly)
+
 
 module.exports = router;
