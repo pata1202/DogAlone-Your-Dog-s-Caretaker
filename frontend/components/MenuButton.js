6f9bb07baae764cc18278cc451c7ec4a5a427e04
chaeyeon1228 <164312022+chaeyeon1228@users.noreply.github.com>
2024-11-29 05:28:29 +0900

    LG기기수정 항목 이동(삭제)

--- frontend/components/MenuButton.js
+++ frontend/components/MenuButton.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import {
-  View,
-  Text,
   TouchableOpacity,
   Image,
-  TextInput,
   StyleSheet,
   Modal,
+  View,
+  Text,
+  TextInput,
+  Alert,
 } from "react-native";
 
 export default function MenuButton() {
@@ -14,21 +15,8 @@ export default function MenuButton() {
   const [isPetInfoModalVisible, setPetInfoModalVisible] = useState(false); // 반려견 정보 수정 모달
   const [isNoticeModalVisible, setNoticeModalVisible] = useState(false); // 공지사항 모달
   const [isInquiryModalVisible, setInquiryModalVisible] = useState(false); // 문의하기 모달
-  const [isDeviceModalVisible, setDeviceModalVisible] = useState(false); // LG 스마트홈 기기 수정 모달
-  const [selectedDevice, setSelectedDevice] = useState(""); // 선택된 LG 기기
-  const [serialNumber, setSerialNumber] = useState(""); // 기기 시리얼 번호
   const [petName, setPetName] = useState(""); // 반려견 이름
   const [petBreed, setPetBreed] = useState(""); // 반려견 종
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // 드롭다운 상태
-
-  const deviceOptions = [
-    "LG 에어컨",
-    "LG 스피커",
-    "LG 티비",
-    "LG 펫케어",
-    "LG 조명",
-    "LG 급수기",
-  ];
 
   const toggleMenu = () => setMenuVisible(!menuVisible);
   const togglePetInfoModal = () =>
@@ -36,29 +24,12 @@ export default function MenuButton() {
   const toggleNoticeModal = () => setNoticeModalVisible(!isNoticeModalVisible);
   const toggleInquiryModal = () =>
     setInquiryModalVisible(!isInquiryModalVisible);
-  const toggleDeviceModal = () => {
-    setDeviceModalVisible(!isDeviceModalVisible);
-    setIsDropdownOpen(false); // 드롭다운 초기화
-    setSelectedDevice(""); // 선택 초기화
-    setSerialNumber(""); // 시리얼 번호 초기화
-  };
-
-  const handleDeviceSelection = (device) => {
-    setSelectedDevice(device);
-    setIsDropdownOpen(false);
-  };
-
-  const saveDeviceInfo = () => {
-    if (selectedDevice && serialNumber) {
-      alert(`기기 추가: ${selectedDevice}\n시리얼 번호: ${serialNumber}`);
-      toggleDeviceModal();
-    } else {
-      alert("기기와 시리얼 번호를 입력해주세요.");
-    }
-  };
 
   const handleSavePetInfo = () => {
-    alert(`반려견 이름: ${petName}\n반려견 종: ${petBreed}`);
+    Alert.alert(
+      "수정완료!", // 팝업 타이틀 변경
+      `반려견 이름: ${petName}\n반려견 종: ${petBreed}` // 내용
+    );
     togglePetInfoModal();
   };
 
@@ -84,15 +55,6 @@ export default function MenuButton() {
           >
             <Text style={styles.menuText}>반려견 정보 수정</Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.menuItem}
-            onPress={() => {
-              toggleDeviceModal();
-              setMenuVisible(false);
-            }}
-          >
-            <Text style={styles.menuText}>LG 스마트홈 기기 수정</Text>
-          </TouchableOpacity>
           <TouchableOpacity style={styles.menuItem} onPress={toggleNoticeModal}>
             <Text style={styles.menuText}>공지사항</Text>
           </TouchableOpacity>
@@ -104,74 +66,6 @@ export default function MenuButton() {
           </TouchableOpacity>
         </View>
       )}
-
-      {/* LG 스마트홈 기기 수정 모달 */}
-      <Modal
-        visible={isDeviceModalVisible}
-        transparent={true}
-        animationType="slide"
-        onRequestClose={toggleDeviceModal}
-      >
-        <View style={styles.modalBackground}>
-          <View style={styles.modalContainer}>
-            <TouchableOpacity
-              style={styles.closeButton}
-              onPress={toggleDeviceModal}
-            >
-              <Image
-                source={require("../dogAloneAssets/closeicon.png")}
-                style={styles.closeButtonImage}
-              />
-            </TouchableOpacity>
-            <Text style={styles.modalTitle}>LG 스마트홈 기기 수정</Text>
-
-            {/* 드롭다운 버튼 */}
-            <TouchableOpacity
-              style={styles.dropdownButton}
-              onPress={() => setIsDropdownOpen(!isDropdownOpen)}
-            >
-              <Text style={styles.dropdownText}>
-                {selectedDevice || "추가할 LG 기기를 선택해주세요"}
-              </Text>
-              <Image
-                source={require("../dogAloneAssets/arrow.png")}
-                style={styles.dropdownIcon}
-              />
-            </TouchableOpacity>
-
-            {/* 드롭다운 리스트 */}
-            {isDropdownOpen && (
-              <View style={styles.dropdownList}>
-                {deviceOptions.map((device, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    style={styles.dropdownItem}
-                    onPress={() => handleDeviceSelection(device)}
-                  >
-                    <Text style={styles.dropdownItemText}>{device}</Text>
-                  </TouchableOpacity>
-                ))}
-              </View>
-            )}
-
-            {/* 시리얼 번호 입력 */}
-            <TextInput
-              style={styles.textInput}
-              placeholder="시리얼 번호를 입력해주세요"
-              value={serialNumber}
-              onChangeText={(text) => setSerialNumber(text)}
-            />
-
-            {/* 저장 버튼 */}
-            <TouchableOpacity
-              style={styles.saveButton}
-              onPress={saveDeviceInfo}
-            >
-              <Text style={styles.saveButtonText}>정보 수정 완료</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
 
       {/* 반려견 정보 수정 모달 */}
       <Modal
@@ -334,41 +228,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 15,
   },
-  dropdownButton: {
-    width: "100%",
-    height: 45,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 10,
-    backgroundColor: "#F4F4F4",
-    borderRadius: 5,
-    marginBottom: 10,
-    borderColor: "#ccc",
-    borderWidth: 1,
-  },
-  dropdownText: {
-    fontSize: 16,
-    color: "#666",
-  },
-  dropdownIcon: {
-    width: 20,
-    height: 20,
-  },
-  dropdownList: {
-    width: "100%",
-    backgroundColor: "#F4F4F4",
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  },
-  dropdownItem: {
-    padding: 10,
-  },
-  dropdownItemText: {
-    fontSize: 16,
-    color: "#333",
-  },
   textInput: {
     width: "100%",
     height: 45,
@@ -382,13 +241,12 @@ const styles = StyleSheet.create({
   saveButton: {
     width: "100%",
     height: 45,
-    backgroundColor: "#007BFF",
+    backgroundColor: "#FAF1C3",
     borderRadius: 5,
     justifyContent: "center",
     alignItems: "center",
   },
   saveButtonText: {
-    color: "#FFFFFF",
     fontWeight: "bold",
     fontSize: 16,
   },
