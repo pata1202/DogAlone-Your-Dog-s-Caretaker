d63f9457d738918f407a09ac8b3e8c5955113e41
chaeyeon1228 <164312022+chaeyeon1228@users.noreply.github.com>
2024-11-29 05:36:44 +0900

    Update smartHome.js

--- frontend/dogAlonePage/smartHome.js
+++ frontend/dogAlonePage/smartHome.js
@@ -1,138 +1,148 @@
-import React, {useState} from 'react';
-import { View, Image, StyleSheet, Text, ScrollView } from 'react-native';
-import LGspeakerCard from '../components/LGspeakerCard';
-import LGtvCard from '../components/LGtvCard';
-import LGlightCard from '../components/LGlightCard';
-import LGairCard from '../components/LGairCard';
-import LGpetCard from '../components/LGpetCard';
-import LGwaterCard from '../components/LGwaterCard';
-import BackButton from '../components/BackButton';
-import HomeButton from '../components/HomeButton';
-import ReportButton from '../components/ReportButton';
-import DocuButton from '../components/DocuButton';
-import MenuButton from '../components/MenuButton';
+import React, { useState } from "react";
+import { View, Image, StyleSheet, Text, ScrollView } from "react-native";
+import LGspeakerCard from "../components/LGspeakerCard";
+import LGtvCard from "../components/LGtvCard";
+import LGlightCard from "../components/LGlightCard";
+import LGairCard from "../components/LGairCard";
+import LGpetCard from "../components/LGpetCard";
+import LGwaterCard from "../components/LGwaterCard";
+import BackButton from "../components/BackButton";
+import HomeButton from "../components/HomeButton";
+import ReportButton from "../components/ReportButton";
+import DocuButton from "../components/DocuButton";
+import MenuButton from "../components/MenuButton";
 
-export default function SmartHome(){
-
-
-return(
+export default function SmartHome() {
+  return (
     <View style={styles.container}>
-       <View style={styles.topbox}>
-      <Text style={styles.toptext}>스마트홈 서비스</Text>
-      <Image source={require('../dogAloneAssets/logo.png')} style={styles.logo} />
-      <BackButton></BackButton>
+      <View style={styles.topbox}>
+        <Text style={styles.toptext}>스마트홈 서비스</Text>
+        <Image
+          source={require("../dogAloneAssets/logo.png")}
+          style={styles.logo}
+        />
+        <BackButton></BackButton>
       </View>
       <Text style={styles.listName}>내 가전 목록</Text>
       <View style={styles.scrollContainer}>
-      <ScrollView contentContainerStyle={styles.scrollCon}>
-
-      <View style={styles.speakerCon}>
-      <LGspeakerCard></LGspeakerCard>        
-      </View>
-      <View style={styles.tvCon}>
-      <LGtvCard></LGtvCard>
-      </View>
-      <View style={styles.petCon}> 
-      <LGpetCard></LGpetCard>
-      </View>
-      <View style={styles.waterCon}> 
-      <LGwaterCard></LGwaterCard>
-      </View>
-      <View style={styles.airCon}>
-      <LGairCard></LGairCard>
-      </View>
-      <View style={styles.lightCon}>
-      <LGlightCard></LGlightCard>
-      </View>
-      
-      </ScrollView>
+        <ScrollView contentContainerStyle={styles.scrollCon}>
+          <View style={styles.speakerCon}>
+            <LGspeakerCard></LGspeakerCard>
+          </View>
+          <View style={styles.tvCon}>
+            <LGtvCard></LGtvCard>
+          </View>
+          <View style={styles.petCon}>
+            <LGpetCard></LGpetCard>
+          </View>
+          <View style={styles.waterCon}>
+            <LGwaterCard></LGwaterCard>
+          </View>
+          <View style={styles.airCon}>
+            <LGairCard></LGairCard>
+          </View>
+          <View style={styles.lightCon}>
+            <LGlightCard></LGlightCard>
+          </View>
+        </ScrollView>
       </View>
       <View style={styles.lineBottom}></View>
-      <HomeButton></HomeButton>
-      <ReportButton></ReportButton>
-      <DocuButton></DocuButton>
-      <MenuButton></MenuButton>
+      <View style={styles.bottomBar}>
+        <HomeButton></HomeButton>
+        <ReportButton></ReportButton>
+        <DocuButton></DocuButton>
+        <MenuButton></MenuButton>
+      </View>
     </View>
-);
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'flex-start',  // 위에서부터 배치 시작
-      alignItems: 'center',           // 가로로 중앙 정렬
-      backgroundColor: '#FFFFFF',     // 배경색 설정
-    },
-    topbox:{
-      width: 393, 
-    height: 60, 
-    backgroundColor: '#FAF1C3', 
-    shadowColor: '#000', 
+  container: {
+    flex: 1,
+    justifyContent: "flex-start", // 위에서부터 배치 시작
+    alignItems: "center", // 가로로 중앙 정렬
+    backgroundColor: "#FFFFFF", // 배경색 설정
+  },
+  topbox: {
+    width: 393,
+    height: 60,
+    backgroundColor: "#FAF1C3",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.2, 
-    shadowRadius: 3, 
-    elevation: 2, 
-    justifyContent: 'center', // 수직 방향 중앙 정렬
-    alignItems: 'center', // 수평 방향 중앙 정렬
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 2,
+    justifyContent: "center", // 수직 방향 중앙 정렬
+    alignItems: "center", // 수평 방향 중앙 정렬
     top: 48,
-    },
-    toptext: {
-      fontSize: 20, 
-      fontFamily: 'Inter', 
-      fontWeight: '500', // Medium에 해당하는 Weight
-      color: '#000000',
-      left:-70,
-      
-    },
-    logo:{
-      position: "absolute",
-      width:60,
-      height:60,
-      right:20,
-    },
-    
-    listName:{
-      fontSize: 18,          
-    fontFamily: "Inter",   
-    fontWeight: "500",    
-    position: "absolute",  // 절대 위치 설정
-    top: 140,              // 상단 여백
-    left: 16,              // 왼쪽 여백
-      
-    },
-    scrollContainer: {
-    position: 'absolute',
+  },
+  toptext: {
+    fontSize: 20,
+    fontFamily: "Inter",
+    fontWeight: "500", // Medium에 해당하는 Weight
+    color: "#000000",
+    left: -70,
+  },
+  logo: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    right: 20,
+  },
+
+  listName: {
+    fontSize: 18,
+    fontFamily: "Inter",
+    fontWeight: "500",
+    position: "absolute", // 절대 위치 설정
+    top: 140, // 상단 여백
+    left: 16, // 왼쪽 여백
+  },
+  scrollContainer: {
+    position: "absolute",
     top: 164, // 큰 컨테이너 기준 상단 여백
     bottom: 777 - 164, // 상단과 하단 사이 영역
-    width: '100%', // 스크롤 컨테이너 너비
+    width: "100%", // 스크롤 컨테이너 너비
     height: 777 - 164, // 스크롤뷰의 높이를 제한
-    backgroundColor: '#FFFFFF',
-    alignItems: 'center',           // 가로로 중앙 정렬
-    },
-    speakerCon: {
-      marginTop: 10,
-    },
-    tvCon: {
-      marginTop: 20,
-    },
-    lightCon: {
-      marginTop: 20,
-    },
-    airCon: {
-      marginTop: 20,
-    },
-    petCon: {
-      marginTop: 20,
-    },
-    waterCon: {
-      marginTop: 20,
-    },
-    lineBottom: {
-      height: 1,
-      width: '100%', // 화면 전체 너비를 차지하도록 설정
-      backgroundColor: '#DADADA', // 회색 선
-      position: 'absolute', // 고정 위치
-      top: 777, // 화면 하단에 고정
-    
-    },
-  });
+    backgroundColor: "#FFFFFF",
+    alignItems: "center", // 가로로 중앙 정렬
+  },
+  speakerCon: {
+    marginTop: 10,
+  },
+  tvCon: {
+    marginTop: 20,
+  },
+  lightCon: {
+    marginTop: 20,
+  },
+  airCon: {
+    marginTop: 20,
+  },
+  petCon: {
+    marginTop: 20,
+  },
+  waterCon: {
+    marginTop: 20,
+  },
+  lineBottom: {
+    height: 1,
+    width: "100%", // 화면 전체 너비를 차지하도록 설정
+    backgroundColor: "#DADADA", // 회색 선
+    position: "absolute", // 고정 위치
+    top: 777, // 화면 하단에 고정
+  },
+  bottomBar: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: 65, // 하단 바 높이
+    backgroundColor: "#FFFFFF", // 흰색
+    justifyContent: "space-around", // 버튼 간 간격 동일
+    flexDirection: "row", // 가로 정렬
+    alignItems: "center",
+    // 경계선 제거
+    borderTopWidth: 0,
+  },
+});
