8fd7e9e1c6b0490d87825388335f585cf13d72c6
chaeyeon1228 <164312022+chaeyeon1228@users.noreply.github.com>
2024-12-04 15:51:23 +0900

    반려견 종류 입력 추가

--- frontend/dogAlonePage/RegisterPage.js
+++ frontend/dogAlonePage/RegisterPage.js
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
-import { View, Image, StyleSheet, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
-import { useFonts, Inter_800ExtraBold } from '@expo-google-fonts/inter';
-import LoginButton from '../components/LoginButton'; // LoginButton 컴포넌트 가져오기
-import { useNavigation } from '@react-navigation/native';
-import BackButton from '../components/BackButton';
-const fetch = require('node-fetch');
-
-
+import React, { useState } from "react";
+import { View, Image, StyleSheet, Text, TextInput, Alert } from "react-native";
+import LoginButton from "../components/LoginButton"; // LoginButton 컴포넌트 가져오기
+import { useNavigation } from "@react-navigation/native";
+import BackButton from "../components/BackButton";
+const fetch = require("node-fetch");
 
 export default function ResgisterPage() {
   // 상태 관리 (useState)
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [dogName, setDogName] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dogName, setDogName] = useState("");
+  const [dogBreed, setDogBreed] = useState("");
   const [isProcessing, setIsProcessing] = useState(false); // 추가된 상태
 
   const navigation = useNavigation();
@@ -21,32 +19,40 @@ export default function ResgisterPage() {
     if (isProcessing) return; // 중복 클릭 방지
     setIsProcessing(true);
 
-    const apiUrl = 'https://6e60-61-82-127-143.ngrok-free.app/register';
+    const apiUrl = "https://6e60-61-82-127-143.ngrok-free.app/register";
     try {
       if (!email || !password || !dogName) {
-        Alert.alert('Error', '모든 필드를 입력해주세요.');
+        Alert.alert("Error", "모든 필드를 입력해주세요.");
         setIsProcessing(false); // 처리 상태 초기화
         return;
       }
 
       const response = await fetch(apiUrl, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password, dogName, dogBreed: 'Shiba Inu' }),
+        body: JSON.stringify({
+          email,
+          password,
+          dogName,
+          dogBreed: "Shiba Inu",
+        }),
       });
 
       if (response.ok) {
         const data = await response.json();
-        Alert.alert('Success', `회원가입이 완료되었습니다! UID: ${data.userId}`);
-        navigation.navigate('Login');
+        Alert.alert(
+          "Success",
+          `회원가입이 완료되었습니다! UID: ${data.userId}`
+        );
+        navigation.navigate("Login");
       } else {
         const error = await response.json();
-        Alert.alert('Error', error.message || '회원가입 실패');
+        Alert.alert("Error", error.message || "회원가입 실패");
       }
     } catch (error) {
-      Alert.alert('Error', '서버와의 연결에 문제가 발생했습니다.');
+      Alert.alert("Error", "서버와의 연결에 문제가 발생했습니다.");
     } finally {
       setIsProcessing(false); // 처리 상태 초기화
     }
@@ -56,7 +62,10 @@ export default function ResgisterPage() {
     <View style={styles.container}>
       <View style={styles.rectangle}></View>
       <Text style={styles.registerText}>sign up</Text>
-      <Image source={require('../dogAloneAssets/logo.png')} style={styles.logo} />
+      <Image
+        source={require("../dogAloneAssets/logo.png")}
+        style={styles.logo}
+      />
       <View style={styles.backBC}>
         <BackButton />
       </View>
@@ -73,7 +82,7 @@ export default function ResgisterPage() {
 
       <View style={styles.inputPW}>
         <TextInput
-          style={styles.textInputPW}
+          style={styles.textInputID}
           placeholder="비밀번호를 입력하세요"
           placeholderTextColor="#B0B0B0"
           secureTextEntry={true}
@@ -82,9 +91,9 @@ export default function ResgisterPage() {
         />
       </View>
 
-      <View style={styles.inputName}>
+      <View style={styles.inputPW}>
         <TextInput
-          style={styles.textInputPW}
+          style={styles.textInputID}
           placeholder="반려견 이름을 입력하세요"
           placeholderTextColor="#B0B0B0"
           value={dogName}
@@ -92,11 +101,21 @@ export default function ResgisterPage() {
         />
       </View>
 
+      <View style={styles.inputPW}>
+        <TextInput
+          style={styles.textInputID}
+          placeholder="반려견 종류를 입력하세요"
+          placeholderTextColor="#B0B0B0"
+          value={dogBreed}
+          onChangeText={setDogBreed}
+        />
+      </View>
+
       <View style={styles.buttonContainer}>
-      <LoginButton
-        title="회원가입 완료"
-        onPress={handleSignUp} // 버튼 클릭 이벤트
-      />
+        <LoginButton
+          title="회원가입 완료"
+          onPress={handleSignUp} // 버튼 클릭 이벤트
+        />
       </View>
     </View>
   );
@@ -105,35 +124,38 @@ export default function ResgisterPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
   },
   loadingContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  buttonContainer: {
+    top: -20,
   },
   rectangle: {
-    position: 'absolute',
+    position: "absolute",
     top: 254,
     left: 23,
     width: 345,
     height: 500,
-    backgroundColor: '#FAF1C3',
+    backgroundColor: "#FAF1C3",
     borderRadius: 30,
   },
   registerText: {
-    position: 'absolute',
-    top: 316,
+    position: "absolute",
+    top: 304,
     fontSize: 30,
-    fontFamily: 'Inter_800ExtraBold',
-    color: '#000000',
-    textAlign: 'center',
+    fontFamily: "Inter_800ExtraBold",
+    color: "#000000",
+    textAlign: "center",
   },
   backBC: {
-    position: 'absolute',
+    position: "absolute",
     top: 260,
     left: 30,
   },
@@ -146,11 +168,11 @@ const styles = StyleSheet.create({
     width: 264.3,
     height: 42.35,
     borderRadius: 7.84,
-    backgroundColor: '#FFFFFF',
-    borderColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: "#FFFFFF",
+    borderColor: "rgba(0, 0, 0, 0.5)",
     borderWidth: 1.57,
     paddingHorizontal: 20,
-    justifyContent: 'center',
+    justifyContent: "center",
     shadowColor: "#000000",
     shadowOpacity: 0.2,
     shadowRadius: 4,
@@ -158,43 +180,23 @@ const styles = StyleSheet.create({
     marginTop: 160,
   },
   textInputID: {
-    fontFamily: 'Roboto',
+    fontFamily: "Roboto",
     fontSize: 11.76,
-    color: '#B0B0B0',
+    color: "#B0B0B0",
   },
   inputPW: {
     width: 264.3,
     height: 42.35,
     borderRadius: 7.84,
-    backgroundColor: '#FFFFFF',
-    borderColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: "#FFFFFF",
+    borderColor: "rgba(0, 0, 0, 0.5)",
     borderWidth: 1.57,
     paddingHorizontal: 20,
-    justifyContent: 'center',
+    justifyContent: "center",
     shadowColor: "#000000",
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 5,
-    marginTop: 40,
-  },
-  inputName: {
-    width: 264.3,
-    height: 42.35,
-    borderRadius: 7.84,
-    backgroundColor: '#FFFFFF',
-    borderColor: 'rgba(0, 0, 0, 0.5)',
-    borderWidth: 1.57,
-    paddingHorizontal: 20,
-    justifyContent: 'center',
-    shadowColor: "#000000",
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-    marginTop: 40,
-  },
-  textInputPW: {
-    fontFamily: 'Roboto',
-    fontSize: 11.76,
-    color: '#B0B0B0',
+    marginTop: 30,
   },
 });
